3942cb8e3966abb26aa755e1d251c9d7f7a99758
NoRIS95 <ryzhikov95@gmail.com>
2023-09-24 09:09:32 +0300

    Third_commit

--- coursesshop/product_app/views.py
+++ coursesshop/product_app/views.py
@@ -1,7 +1,7 @@
 from rest_framework import  status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from rest_framework.views import APIView
 from .models import Product, Lesson, User, WatchStatuses, View
 from .serializer import ProductSerializer, LessonSerializer, UserSerializer, WatchStatusesSerializer, ViewSerializer
 
